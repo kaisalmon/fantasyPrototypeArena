5996a39583f7892e4c729fa3183b0772bdb5bcaf
Kai Salmon <kaisalmon@hotmail.com>
2017-11-19 19:31:40 +0100

    Created python util for generating json/icons.json

---
+++ json/create_icons_json.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+import json
+from pprint import pprint
+
+parser = argparse.ArgumentParser(description='Create the json file of download locations for icons, given a multilayer directory of icons.')
+
+parser.add_argument('path',
+                   help='The path of the icons, downloaded from http://game-icons.net')
+parser.add_argument('output',
+                   help='The json file to store the mapping')
+
+args = parser.parse_args()
+path = args.path
+
+output = {}
+
+for root, dirs, files in os.walk(path, topdown=False):
+    for name in files:
+        icon = name.split('.')[0]
+        rel = (os.path.join(root, name))
+        rel = rel.replace(path,"http://game-icons.net/icons/")
+        output[icon] =  rel
+
+with open(args.output, 'w') as outfile:
+    json.dump(output, outfile, indent=4, sort_keys=True)
